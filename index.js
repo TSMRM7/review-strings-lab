ee6cb1427e5b0f230cedca2d709969a8a0dcce59
ryanruri <muiruri13ryan@gmail.com>
2024-09-23 11:30:40 +0300

    initial git

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = "Ryan"
+const welcomeMessage = "Welcome to Flatbook, Ryan !"
+console.log(`${currentUser} ${welcomeMessage}`)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = "Welcome, R !"
+
